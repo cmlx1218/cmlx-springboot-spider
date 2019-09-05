e54f27bde8b1b7fdb5f09b13d2abce2456b8731c
cmlx <cmlx1218@163.com>
2019-09-05 09:10:22 +0800

    .feat:添加模拟浏览器访问网页

--- src/main/java/com/cmlx/spider/utils/HttpUtil.java
+++ src/main/java/com/cmlx/spider/utils/HttpUtil.java
@@ -12,7 +12,8 @@ import java.util.concurrent.TimeUnit;
 @UtilityClass
 public class HttpUtil {
     private static OkHttpClient okHttpClient;
-    private static int num = 0;
+    private static String USER_AGENT = "User-Agent";
+    private static String USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0";
 
     static {
         okHttpClient = new OkHttpClient.Builder()
@@ -25,6 +26,7 @@ public class HttpUtil {
         //创建连接客户端
         Request request = new Request.Builder()
                 .url(path)
+                .header(USER_AGENT,USER_AGENT_VALUE)
                 .build();
         //创建"调用" 对象
         Call call = okHttpClient.newCall(request);
